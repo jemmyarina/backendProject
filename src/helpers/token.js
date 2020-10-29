5dbc1bdd2c3e7e3867fac8ca6e92de2f0f3bb503
jemmyarina <jemmyarinau@gmail.com>
2020-10-29 23:43:33 +0200

    new changes for TRAVIS testing

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -5,7 +5,7 @@ import config from '../config/config';
     const {firstName, lastName, email, _id}=user;
     const secreteKey = config.SECRETE_KEY;
 
-    return jwt.sign({firstName, lastName, email, _id}, secreteKey, { expiresIn: '600s' });
+    return jwt.sign({firstName, lastName, email, _id}, secreteKey, { expiresIn: '3d' });
 }
 
 export default generateToken;
